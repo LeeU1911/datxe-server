2eaf166a3dd55d551593a77964c28c91824f5a83
Le Tan Vu <imleeu@gmail.com>
2016-01-20 21:47:55 +0700

    add socket.io

--- OrderItServer.js
+++ OrderItServer.js
@@ -11,10 +11,21 @@ app.post('/', function (req, res) {
 	req.on('data', function(data){
 		console.log(data);
 	});
-	
+
   res.send('{"response":"success"}');
 });
 
-app.listen(PORT, function () {
+var server = app.listen(PORT, function () {
   console.log('OrderIt app listening on port %s!', PORT);
 })
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected with id %s', socket.id);
+  io.emit('handshake', 'it\'s me, server');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
